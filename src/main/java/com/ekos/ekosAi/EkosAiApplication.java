0795570c8fc941c1c828a516e20cca2534683aac
Osman Ozden <ozden.osman@hotmail.com>
2022-08-25 15:30:08 +0300

    first com

---
+++ src/main/java/com/ekos/ekosAi/EkosAiApplication.java
@@ -0,0 +1,13 @@
+package com.ekos.ekosAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkosAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkosAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekos/ekosAi/EkosAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekos.ekosAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkosAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
