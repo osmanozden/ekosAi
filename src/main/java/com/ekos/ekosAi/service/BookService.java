ef6c9f50f85d7f14dc793e68ed9aa0f255716e45
Osman Ozden <ozden.osman@hotmail.com>
2022-08-25 17:16:18 +0300

    Service layer , User and Book service added.

---
+++ src/main/java/com/ekos/ekosAi/service/BookService.java
@@ -0,0 +1,9 @@
+package com.ekos.ekosAi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BookService {
+}
